4f0ee4bc4e2bd241ec8b2a10b72ef657c012b3a2
Karin Ochayon <karinoch070@gmail.com>
2023-05-18 12:08:53 +0300

    More changes in the design

--- src/ChatApp.java
+++ src/ChatApp.java
@@ -21,7 +21,7 @@ public class ChatApp {
         mainFrame.setLayout(new BorderLayout());
         mainFrame.setResizable(false); // Disable window resizing
 
-        backgroundPanel = new BackgroundPanel("background.png");
+        backgroundPanel = new BackgroundPanel("images/background.png");
         mainFrame.add(backgroundPanel);
 
         JButton startButton = new JButton("Start");
@@ -79,52 +79,90 @@ public class ChatApp {
 
         // Create the chat page components
         JPanel chatPanel = new JPanel(new BorderLayout());
+        chatPanel.setBackground(Color.WHITE); // Set background color
 
-        // Top Panel: Server, Port, Connect, and Disconnect
-        JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        topPanel.setBackground(new Color(252, 220, 229)); // Set background color
+        // Top Panel: Server, Port, Connect, Disconnect, and Title
+        JPanel topPanel = new JPanel(new BorderLayout());
+//        topPanel.setBackground(new Color(220, 220, 220)); // Set background color
+        topPanel.setBorder(new EmptyBorder(20, 20, 20, 20));
+
+        // Title Image
+        ImageIcon titleIcon = new ImageIcon(getClass().getResource("images/title.png"));
+        JLabel titleLabel = new JLabel(titleIcon);
+
+        // Server, Port, Connect, and Disconnect
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 10));
+//        buttonPanel.setBackground(new Color(220, 220, 220)); // Set background color
+
+        buttonPanel.setBackground(Color.white);
 
         JLabel serverLabel = new JLabel("Server:");
         JTextField serverField = new JTextField(10);
+        serverField.setBackground(Color.white);
 
         JLabel portLabel = new JLabel("Port:");
         JTextField portField = new JTextField(10);
+        portField.setBackground(Color.white);
 
         JButton connectButton = new JButton("Connect");
-        connectButton.setBackground(new Color(120, 8, 247)); // Set background color
+        connectButton.setBackground(new Color(120, 9, 247)); // Set background color
         connectButton.setForeground(Color.WHITE); // Set text color
+        connectButton.setFont(new Font("Arial", Font.BOLD, 14)); // Set font
         connectButton.setEnabled(false);
 
         JButton disconnectButton = new JButton("Disconnect");
-        disconnectButton.setBackground(new Color(120, 8, 247)); // Set background color
+        disconnectButton.setBackground(new Color(120, 9, 247)); // Set background color
         disconnectButton.setForeground(Color.WHITE); // Set text color
+        disconnectButton.setFont(new Font("Arial", Font.BOLD, 14)); // Set font
         disconnectButton.setEnabled(false);
 
-        topPanel.add(serverLabel);
-        topPanel.add(serverField);
-        topPanel.add(portLabel);
-        topPanel.add(portField);
-        topPanel.add(connectButton);
-        topPanel.add(disconnectButton);
+        buttonPanel.add(serverLabel);
+        buttonPanel.add(serverField);
+        buttonPanel.add(portLabel);
+        buttonPanel.add(portField);
+        buttonPanel.add(connectButton);
+        buttonPanel.add(disconnectButton);
+
+        topPanel.add(titleLabel, BorderLayout.NORTH);
+        topPanel.add(buttonPanel, BorderLayout.CENTER);
+        topPanel.setBackground(Color.white);
 
         // Middle Panel: Chat Messages
         JTextArea chatMessagesArea = new JTextArea();
         chatMessagesArea.setEditable(false);
+
+        chatMessagesArea.setBackground(Color.WHITE); // Set background color
+        chatMessagesArea.setFont(new Font("Arial", Font.PLAIN, 14)); // Set font
         JScrollPane chatScrollPane = new JScrollPane(chatMessagesArea);
 
         // Bottom Panel: User Input, Recipient ComboBox, and Send Button
         JPanel bottomPanel = new JPanel(new BorderLayout());
+//        bottomPanel.setBackground(new Color(220, 220, 220)); // Set background color
+        bottomPanel.setBorder(new EmptyBorder(10, 20, 20, 20));
+
         JTextField userInputField = new JTextField();
+        userInputField.setPreferredSize(new Dimension(400, 30)); // Set preferred size
+
         JComboBox<String> recipientComboBox = new JComboBox<>();
+        recipientComboBox.setPreferredSize(new Dimension(120, 30)); // Set preferred size
+        recipientComboBox.setBackground(Color.white);
+
         JButton sendButton = new JButton("Send");
+        sendButton.setBackground(new Color(120, 9, 247)); // Set background color
+        sendButton.setForeground(Color.WHITE); // Set text color
+        sendButton.setFont(new Font("Arial", Font.BOLD, 14)); // Set font
+
         bottomPanel.add(userInputField, BorderLayout.CENTER);
         bottomPanel.add(recipientComboBox, BorderLayout.WEST);
         bottomPanel.add(sendButton, BorderLayout.EAST);
+        bottomPanel.setBackground(Color.white);
 
         // Add components to the chat panel
         chatPanel.add(topPanel, BorderLayout.NORTH);
         chatPanel.add(chatScrollPane, BorderLayout.CENTER);
         chatPanel.add(bottomPanel, BorderLayout.SOUTH);
+
+
 
         // Add chatPanel to the frame
         mainFrame.add(chatPanel);
